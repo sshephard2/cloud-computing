f94b6eea6de4c04d27381e319cd763d7c8593e42
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2016-12-05 09:18:41 +0000

    Create Sighting message class

---
+++ src/main/java/uk/ac/ncl/smartcam/Sighting.java
@@ -0,0 +1,101 @@
+package uk.ac.ncl.smartcam;
+
+import java.util.Date;
+
+/**
+ * Smart Camera Sighting message object
+ * @author Stephen Shephard
+ *
+ */
+public class Sighting {
+	
+	private Long id;
+	private Date timestamp;
+	private String registration;
+	private String vehicletype;
+	private int speed;
+
+	/**
+	 * @param id
+	 * @param registration
+	 * @param vehicletype
+	 * @param speed
+	 */
+	public Sighting(Long id, String registration, String vehicletype, int speed) {
+		this.id = id;
+		this.timestamp = new Date();
+		this.registration = registration;
+		this.vehicletype = vehicletype;
+		this.speed = speed;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the timestamp
+	 */
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * @return the registration
+	 */
+	public String getRegistration() {
+		return registration;
+	}
+
+	/**
+	 * @param registration the registration to set
+	 */
+	public void setRegistration(String registration) {
+		this.registration = registration;
+	}
+
+	/**
+	 * @return the vehicletype
+	 */
+	public String getVehicletype() {
+		return vehicletype;
+	}
+
+	/**
+	 * @param vehicletype the vehicletype to set
+	 */
+	public void setVehicletype(String vehicletype) {
+		this.vehicletype = vehicletype;
+	}
+
+	/**
+	 * @return the speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+}
