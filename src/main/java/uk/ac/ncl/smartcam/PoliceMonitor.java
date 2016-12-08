b0698543a1379dbdd0c9fefb26b15b6e2911d7d7
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2016-12-08 17:23:16 +0000

    Begin implementing PoliceMonitor

---
+++ src/main/java/uk/ac/ncl/smartcam/PoliceMonitor.java
@@ -0,0 +1,124 @@
+package uk.ac.ncl.smartcam;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import com.microsoft.azure.storage.table.TableEntity;
+
+import uk.ac.ncl.smartcam.Sighting;
+import uk.ac.ncl.smartcam.SpeedingVehicle;
+import uk.ac.ncl.smartcam.ServiceBus;
+import uk.ac.ncl.smartcam.TableStorage;
+
+public class PoliceMonitor {
+	
+	private static final String Speeding = "Speeding";
+	private static final long BatchLimit = 100L;
+	private static final int MaxPollTime = 60; // maximum time between polls in seconds
+	
+	public static void main(String[] args) {
+		
+		// Connect to Azure Service Bus
+		ServiceBus service;
+		try {
+			service = new ServiceBus();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		// Connect to Azure Table Storage
+		TableStorage tableService;
+		try {
+			tableService = new TableStorage();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		Object message;
+		Long messageCount;
+		
+		// Create HashMaps of Queues for SpeedingVehicles
+		// This allows a queue to be created for every value of partition key
+		// and ensures that when we use batch insert, we send the queue for each key
+		// i.e. all the entities in the batch have the same partition key
+		Map<String, Queue<TableEntity>> speedingVehicles = new HashMap<String, Queue<TableEntity>>();
+
+		// Run until interrupted
+		boolean running = true;
+		
+		while(running) {
+			
+			// How many messages in the subscription?
+			messageCount = service.messageCount(Speeding);
+			System.out.println("Subscription " + Speeding + ":" + messageCount + " messages");
+			
+			// Clear the maps
+			speedingVehicles.clear();
+			
+			// Read in the messages (up to the batch limit)
+			for (Long i=0L; i<messageCount && i<BatchLimit; i++) {
+				message = service.receiveMessage(Speeding);
+
+				if (message instanceof Sighting) {
+					System.out.println("Sighting:" + message.toString());
+					
+					// Convert Sighting message to SpeedingVehicle entity
+					Sighting sight = (Sighting)message;
+					Long id = sight.getId();
+					Date timestamp = sight.getTimestamp();
+					String registration = sight.getRegistration();
+					String vehicletype = sight.getVehicletype();
+					int speed = sight.getSpeed();
+					int speedlimit = sight.getSpeedlimit();
+					SpeedingVehicle speeding = new SpeedingVehicle(id, registration, vehicletype, speed, speedlimit);				
+					
+					// Is the vehicle speed more than 10% over the speedlimit?
+					// Using integer arithmetic, if 11*speed>10*speedlimit
+					// Then speed is greater than speedlimit*1.1
+					if (10*speeding.getSpeed() > 11*speeding.getSpeedlimit()) {
+						speeding.setPriority(true);
+					}
+					
+					// Insert into sightings map of queues
+					String partitionKey = speeding.getPartitionKey();
+					// If there isn't already a queue for this partition key, create one
+					Queue<TableEntity> queue = speedingVehicles.get(partitionKey);
+					if (queue == null) {
+						queue = new LinkedList<TableEntity>();
+						speedingVehicles.put(partitionKey, queue);
+					}
+					queue.add((Sighting)message);
+					
+				} else {
+					// Do nothing
+				}
+			}
+					
+			// Partition keys and records in each?
+			for (String key: speedingVehicles.keySet()) {
+				System.out.println("SpeedingVehicle key " + key + "," + speedingVehicles.get(key).size() + " values");
+			}
+						
+			// If there are any speedingvehicles queued, then insert them
+//			for (Queue<TableEntity> queue: speedingVehicles.values()) {
+//				tableService.batchInsert("sightings", queue);
+//			}
+			
+			try {
+				// Simple formula that determines the polling time based on how many messages we last received
+				// If we had a full maximum batch, then polling time is zero
+				// If we had no messages, we wait the maximum polling time
+				Thread.sleep(MaxPollTime*10*Math.max(0, BatchLimit-messageCount));
+			} catch (InterruptedException e) {
+				running = false;
+			}
+		}
+		
+	}
+
+}
---
+++ src/main/java/uk/ac/ncl/smartcam/SpeedingVehicle.java
@@ -0,0 +1,184 @@
+package uk.ac.ncl.smartcam;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.microsoft.azure.storage.table.TableServiceEntity;
+
+/**
+ * Smart Camera Speeding Vehicle entity object
+ * @author Stephen Shephard
+ *
+ */
+public class SpeedingVehicle extends TableServiceEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private Date timestamp;
+	private String registration;
+	private String vehicletype;
+	private int speed;
+	private int speedlimit;
+	private boolean priority;
+
+	/**
+	 * No-arg constructor for Jackson JSON serialization
+	 */
+	public SpeedingVehicle() {
+		// Do nothing
+	}
+	
+	/**
+	 * Constructor for Smart Camera Sighting message
+	 * @param id camera id
+	 * @param registration vehicle registration number
+	 * @param vehicletype type of vehicle Car|Truck|Motorcycle
+	 * @param speed speed of vehicle in mph
+	 * @param speedlimit speed limit in mph
+	 */
+	public SpeedingVehicle(Long id, String registration, String vehicletype, int speed, int speedlimit) {
+		this.id = id;
+		this.timestamp = new Date();
+		this.registration = registration;
+		this.vehicletype = vehicletype;
+		this.speed = speed;
+		this.speedlimit = speedlimit;
+		this.partitionKey = vehicletype ; // PartitionKey
+		this.rowKey = registration + Long.toString(timestamp.getTime()); // RowKey
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the timestamp
+	 */
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = (Date)timestamp.clone();
+	}
+
+	/**
+	 * @return the registration
+	 */
+	public String getRegistration() {
+		return registration;
+	}
+
+	/**
+	 * @param registration the registration to set
+	 */
+	public void setRegistration(String registration) {
+		this.registration = registration;
+	}
+
+	/**
+	 * @return the vehicletype
+	 */
+	public String getVehicletype() {
+		return vehicletype;
+	}
+
+	/**
+	 * @param vehicletype the vehicletype to set
+	 */
+	public void setVehicletype(String vehicletype) {
+		this.vehicletype = vehicletype;
+	}
+
+	/**
+	 * @return the speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	/**
+	 * @return the speedlimit
+	 */
+	public int getSpeedlimit() {
+		return speedlimit;
+	}
+	
+	/**
+	 * @param speedlimit the speedlimit to set
+	 */
+	public void setSpeedlimit(int speedlimit) {
+		this.speedlimit = speedlimit;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public boolean isPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(boolean priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the partitionKey
+	 */
+	public String getPartitionKey() {
+		return partitionKey;
+	}
+
+	/**
+	 * @param partitionKey the partitionKey to set
+	 */
+	public void setPartitionKey(String partitionKey) {
+		this.partitionKey = partitionKey;
+	}
+
+	/**
+	 * @return the rowKey
+	 */
+	public String getRowKey() {
+		return rowKey;
+	}
+
+	/**
+	 * @param rowKey the rowKey to set
+	 */
+	public void setRowKey(String rowKey) {
+		this.rowKey = rowKey;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Sighting [id=" + id + ", timestamp=" + timestamp + ", registration=" + registration + ", vehicletype="
+				+ vehicletype + ", speed=" + speed + "]";
+	}
+}
